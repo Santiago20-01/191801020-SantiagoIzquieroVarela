468d4dbd99a447f585cddd81753dacfa51d53b62
Santiago <santiago.izquierdo1820@uppuebla.edu.mx>
2021-01-25 09:45:12 -0600

    Tacos

--- Tacos/src/main/java/Tacos.java
+++ Tacos/src/main/java/Tacos.java
@@ -14,6 +14,14 @@ import java.util.Scanner;
 public class Tacos {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Igrese su primer calificacion: ");
+        System.out.println("\t Menu");
+        System.out.println("-Tacos $17");
+        System.out.println("-Refrescos $10");
+        System.out.println("¿Cuantos tacos consumio?");
+        int tacos=scanner.nextInt();
+        System.out.println("¿Cuantos refrescos consumio?");
+        int refrescos=scanner.nextInt();
+        System.out.println("Cuenta:");
+        System.out.println((tacos*17)+(refrescos*10));
     }
 }
