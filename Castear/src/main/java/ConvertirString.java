8faf5dbc9a5de71f9dfae34404d13cf5bd684957
Santiago <santiago.izquierdo1820@uppuebla.edu.mx>
2021-01-29 08:45:52 -0600

    NumeroAleatoreo

---
+++ Castear/src/main/java/ConvertirString.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ramse
+ */
+public class ConvertirString {
+    public static void main(String[] args) {
+        String numero="2";
+        int numeroReal=Integer.parseInt(numero);
+        System.out.println(numeroReal);
+        int resultado=numeroReal+5;
+        String resultadoFinal="El resultado es"+resultado;
+    }
+}
